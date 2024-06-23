dbf36179d01f7173c7df1528869580d51d29c4d4
vikashkumar6207 <vktd828403@gmail.com>
2024-06-23 22:53:33 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import '../styles/App.css';
+import programming from '../assets/programming-gif';
 
 const GIF = () => {
   return (
-    <img className='import-image' height={200}/>
+    <img className='import-image' src={programming} alt='programming-gif' height={200}/>
   )
 }
 
 const App = () => {
 
+  const urlImage = 'https://picsum.photos/id/400/200';
+
   return (
     <div id="main">
       <h1>Add images in 2-different ways.</h1>
       <GIF />
-      <img className='url-image' />
+      <img className='url-image' src={urlImage} alt='A random image' />
     </div>
   )
 }
