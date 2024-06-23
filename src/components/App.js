9a1bcf08afa993346aba2ae1ac120bbe88d2647b
vikashkumar6207 <vktd828403@gmail.com>
2024-06-23 22:55:50 +0530

    don

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
-import programming from '../assets/programming-gif';
+import programmingGif from '../assets/programming.gif'; // Adjust the path according to your project structure
 
 const GIF = () => {
   return (
-    <img className='import-image' src={programming} alt='programming-gif' height={200}/>
+    <img 
+      className='import-image' 
+      src={programmingGif} 
+      alt="programming-gif" 
+      height={200} 
+    />
   )
 }
 
 const App = () => {
-
-  const urlImage = 'https://picsum.photos/id/400/200';
+  const imageUrl = "https://picsum.photos/id/400/200";
 
   return (
     <div id="main">
       <h1>Add images in 2-different ways.</h1>
       <GIF />
-      <img className='url-image' src={urlImage} alt='A random image' />
+      <img 
+        className='url-image' 
+        src={imageUrl} 
+        alt="A random image" 
+      />
     </div>
   )
 }
-
 
 export default App;
